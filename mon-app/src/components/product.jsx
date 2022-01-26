390d6484de3540c2477e9735c75264a7495705ca
ByZoom <by.zoom69@gmail.com>
2022-01-26 22:10:02 +0100

    LETS GOOO

---
+++ mon-app/src/components/product.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { button } from 'react-bootstrap';
+
+function Product(props) {
+  let {
+    product
+  } = props;
+
+  return (
+    ""
+  );
+}
+
+Product.propTypes = {
+  product: React.PropTypes.object.isRequired
+};
+
+export default Product;
