246586a45b7057c80f1ad9df0a984e940580be2b
ByZoom <by.zoom69@gmail.com>
2022-01-26 23:14:42 +0100

    affichage liste produit

--- mon-app/src/components/list_product.jsx
+++ mon-app/src/components/list_product.jsx
@@ -1,3 +1,19 @@
 import react from "react";
 import product_card from "../data/product_data";
 
+function list_product(product_card) {
+    const list_items = product_card.map((item) => 
+    <ul>
+        <li>{item.product_name}</li>
+        <li>{item.price}</li>
+        <li>{item.description}</li>
+    </ul>
+    );
+    return (
+        <div>
+            {list_items}
+        </div>
+    );
+  }
+
+export default list_product;
